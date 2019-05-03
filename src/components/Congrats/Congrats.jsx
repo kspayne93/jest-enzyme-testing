02d9412a79793107216f3cbfe050bc73a78c147d
Kyle Payne <kspayne93@gmail.com>
2019-05-03 11:09:32 -0600

    added bootstrap styling and rendered components

--- src/components/Congrats/Congrats.jsx
+++ src/components/Congrats/Congrats.jsx
@@ -12,7 +12,7 @@ const Congrats = (props) => {
       <>
          {
             props.success ?
-            <div data-test='component-congrats'>
+            <div data-test='component-congrats' className='alert alert-success'>
                <span data-test='congrats-message'>
                   Congratulations! You guessed the word!
                </span>
